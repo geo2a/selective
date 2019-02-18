8b0e39e7a19b238611503ecc45e75c399567f695
Georgy Lukyanov <georgiylukjanov@gmail.com>
2019-02-18 13:04:15 +0000

    More unfinished experiments.

--- src/Control/Selective/Free/Examples/FS.hs
+++ src/Control/Selective/Free/Examples/FS.hs
@@ -28,14 +28,26 @@ instance Functor (Coyoneda f) where
 -- showCF (Coyoneda (R k) t)   = "Read " ++ show k
 -- showCF (Coyoneda (W k v) t) = "Write " ++ show k ++ " " ++ showCF v
 
-r :: k -> Coyoneda (F k v) v
-r k = Coyoneda (R k) id
+-- | Coyoneda turns the effect signature into a 'Functor'
+--   so the 'KVStateF' datatype may be used in Free constructions
+newtype FS k v a = FS (Coyoneda (F k v) a)
+    deriving Functor
 
-w :: k -> Coyoneda (F k v) a -> Coyoneda (F k v) a
-w k (Coyoneda (R kk  ) y) = Coyoneda (W k (R kk  )) y
-w k (Coyoneda (W kk x) y) = Coyoneda (W k (W kk x)) y
+showFS :: (Show k, Show v) => FS k v a -> String
+showFS (FS (Coyoneda (R k) t))    = "Read " ++ show k
+showFS (FS (Coyoneda (W k fv) t)) = "Write " ++ show k -- ++ " " ++ showFS fv
 
-type Program k v a = Select (Coyoneda (F k v)) a
+instance (Show k, Show v) => Show (FS k v a) where
+    show x = showFS x
+
+r :: k -> FS k v v
+r k = FS $ Coyoneda (R k) id
+
+w :: k -> FS k v a -> FS k v a
+w k (FS (Coyoneda (R kk  ) y)) = FS $ Coyoneda (W k (R kk  )) y
+w k (FS (Coyoneda (W kk x) y)) = FS $ Coyoneda (W k (W kk x)) y
+
+type Program k v a = Select (FS k v) a
 
 -- | Embed a 'Read' command into a 'Program'
 read :: k -> Program k v v
@@ -43,16 +55,16 @@ read k = liftSelect (r k)
 
 -- | Embed a 'Write' command into a 'Program'
 write :: k -> Program k v v -> Program k v v
-write k = runSelect (liftSelect . w k)
+write k fv = runSelect (liftSelect . w k) fv
 
 -- | Interpret the mutable dictionary effect in 'MonadState'
-toState :: (Ord k, S.MonadState (Map.Map k v) m) => Coyoneda (F k v) a -> m a
-toState ((Coyoneda (R k) t)) =
+toState :: (Ord k, S.MonadState (Map.Map k v) m) => FS k v a -> m a
+toState (FS (Coyoneda (R k) t)) =
     let s = S.get
     in t <$> ((Map.!) <$> s <*> pure k)
-toState ((Coyoneda (W k fv) t)) = do
+toState (FS (Coyoneda (W k fv) t)) = do
     s <- S.get
-    let (v, s') = S.runState (toState (Coyoneda fv id)) s
+    let (v, s') = S.runState (toState (FS $ Coyoneda fv id)) s
         s'' = Map.insert k v s'
     S.put s''
     t <$> pure v
@@ -65,12 +77,21 @@ add :: Program String Int Int
 add = let x = read "x"
           y = read "y"
           sum = (+) <$> x <*> y
-          isZero = (==) <$> sum <*> pure 0
-      in ifS isZero (write "sumIsZero" (pure 1))
-                    (write "sumIsZero" (pure 0)) *> sum
+        --   isZero = (==) <$> sum <*> pure 0
+      in write "z" sum
+         -- ifS isZero (write "sumIsZero" (pure 1))
+         --            (write "sumIsZero" (pure 0)) *> sum
 
-runAdd :: (Int, Map.Map String Int)
-runAdd = runProgramState add (Map.fromList [("x", -1), ("y", 1), ("sumIsZero", 0)])
+addAndWrite :: Program String Int Int
+addAndWrite =
+    let x = read "x"
+        y = read "y"
+        sum = (+) <$> x <*> y
+    in write "z" sum
+
+runExample :: Program String Int Int -> (Int, Map.Map String Int)
+runExample prog = runProgramState prog
+    (Map.fromList [("x", 5), ("y", 6), ("sumIsZero", 0)])
 
 -- | Inspect add's effects
 --   > analyseAdd
---
+++ src/Control/Selective/Free/Examples/RW2.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Control.Selective.Free.Examples.RW2 where
+
+import Prelude hiding (read)
+import qualified Control.Monad.State as S
+import Control.Selective
+import Control.Selective.Free
+-- import Data.Functor.Coyoneda
+import Data.Functor.Const
+import Data.Either (partitionEithers)
+import qualified Data.Map.Strict as Map
+
+data RW k v a = R k                 (v -> a)
+              | W k (Program k v v) (v -> a)
+    deriving Functor
+
+instance Show k => Show (RW k v a) where
+    show (R k _)   = "R " ++ show k
+    show (W k _ _) = "W " ++ show k
+
+type Program k v a = Select (RW k v) a
+
+-- | Embed a 'Read' command into a 'Program'
+read :: k -> Program k v v
+read k = liftSelect $ R k id
+
+-- | Embed a 'Write' command into a 'Program'
+write :: k -> Program k v v -> Program k v v
+write k p = liftSelect $ W k p id
+
+add :: Program String Int Int
+add = let x = read "x"
+          y = read "y"
+          sum = (+) <$> x <*> y
+        --   isZero = (==) <$> sum <*> pure 0
+      in write "z" sum
+         -- ifS isZero (write "sumIsZero" (pure 1))
+         --            (write "sumIsZero" (pure 0)) *> sum
+
+addAndWrite :: Program String Int Int
+addAndWrite =
+    let x = read "x"
+        y = read "y"
+        sum = (+) <$> x <*> y
+    in write "z" sum
